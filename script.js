3cefe64f32f472f8b7c9d37f5ac66ecd0e34c712
Erick J. Pineda Amézquita <50930139+epinedaWorks@users.noreply.github.com>
2024-11-12 01:46:36 -0600

    a

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-<script
-  type="text/javascript"
-  src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
-</script>
-<script type="text/javascript">
-  (function () {
-    emailjs.init({
-      publicKey: 'RO81h33TUhbyarOAS',
-    });
-  })();
-</script>
 
 import emailjs from '@emailjs/browser';
 
