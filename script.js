37b6a2c7bb822c2e9cce450633c868cce1ae780f
Erick J. Pineda Amézquita <50930139+epinedaWorks@users.noreply.github.com>
2024-11-11 23:18:55 -0600

    Add contact, update conduct code and email sender
    Add contact, update conduct code and email sender

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ window.onscroll = function() {
         button.style.display = 'none';
     }
 };
+
+// Inicializar EmailJS (reemplace 'YOUR_USER_ID' con su User ID)
+emailjs.init('aYItPsEdpLFK12-IL');
+
+// Manejar el envío del formulario
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+    const statusMessage = document.getElementById('statusMessage');
+
+    // Enviar el formulario utilizando EmailJS
+    emailjs.sendForm('service_qphkm8d', 'plantillaContacto', '#contactForm')
+        .then(() => {
+            statusMessage.textContent = '¡Mensaje enviado exitosamente!';
+            statusMessage.style.color = 'green';
+            document.getElementById('contactForm').reset(); // Limpiar el formulario
+        }, (error) => {
+            statusMessage.textContent = 'Error al enviar el mensaje. Inténtelo de nuevo.';
+            statusMessage.style.color = 'red';
+            console.error('Error:', error);
+        });
+});
